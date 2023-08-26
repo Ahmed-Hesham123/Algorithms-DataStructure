51055f523158061876816c7620e076cc6e1b75c2
Ahmed Hesham <ahmedh246891@gmail.com>
2023-08-26 19:01:29 +0300

    A new problem has been added

---
+++ LeetCode_PS/7_Longest_Common_Prefix/main.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 0) {
+    return "";
+  }
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, prefix.length - 1);
+      if (prefix === "") {
+        return "";
+      }
+    }
+  }
+  return prefix;
+};
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+console.log(longestCommonPrefix(["dog", "racecar", "car"]));
+
+// Big-O = O(n^2) - Logarithmic
---
+++ LeetCode_PS/7_Longest_Common_Prefix/readme.md
@@ -0,0 +1,24 @@
+# Longest Common Prefix
+
+## Problem:
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string `""`.
+
+## Examples
+
+### Example 1:
+
+    Input: strs = ["flower","flow","flight"]
+    Output: "fl"
+
+### Example 2:
+
+    Input: strs = ["dog","racecar","car"]
+    Output: ""
+    Explanation: There is no common prefix among the input strings.
+
+$$Big-O = O(n^2) - Logarithmic$$
+
+[!Problem Link](https://leetcode.com/problems/longest-common-prefix/)
