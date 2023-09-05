342505370aa79c09a6fb70ac11825f1e7f567fd1
Ahmed Hesham <ahmedh246891@gmail.com>
2023-09-05 08:56:20 +0300

    A new problem has been added

---
+++ LeetCode_PS/14_Implement_Stack_using_Queues/main.js
@@ -0,0 +1,29 @@
+class MyStack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  top() {
+    return this.items[this.items.length - 1];
+  }
+
+  empty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.toString());
+  }
+}
