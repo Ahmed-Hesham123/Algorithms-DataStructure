296be77ff5861bafd3362676015ce96b3920707f
Ahmed Hesham <ahmedh246891@gmail.com>
2023-09-11 10:09:47 +0300

    A new Problem has been added

---
+++ LeetCode_PS/Thirty_Daya_With_JS/5_Apply_Transform_Over_Each_Element_in_Array/main.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let newArr = [];
+    for(let i=0; i<arr.length; i++){
+        newArr[i] = fn(arr[i], i);
+    }
+    return newArr;
+};
+
+// Big-O = O(n) - Linear
