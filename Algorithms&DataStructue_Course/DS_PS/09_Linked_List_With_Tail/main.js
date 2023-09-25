54d1eaa1444cb722783fd1ea8e7f2532dcec7c2f
Ahmed Hesham <ahmedh246891@gmail.com>
2023-09-25 23:44:12 +0300

    A new DS has been added

---
+++ Algorithms&DataStructue_Course/DS_PS/09_Linked_List_With_Tail/main.js
@@ -0,0 +1,108 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is empty");
+    } else {
+      let curr = this.head;
+      let listValues = "";
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(listValues);
+    }
+  }
+
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.size++;
+  }
+
+  removeFromFront() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    this.size--;
+    return value;
+  }
+
+  removeFromEnd() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const value = this.tail.value;
+    if (this.size === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let prev = this.head;
+      while (prev.next !== this.tail) {
+        prev = prev.next;
+      }
+      prev.next = null;
+      this.tail = prev;
+    }
+    this.size--;
+    return value;
+  }
+}
+
+const list = new LinkedList();
+console.log("List is empty? ", list.isEmpty());
+console.log("List Size: ", list.getSize());
+list.print();
+console.log("=========");
+list.prepend(30);
+list.prepend(10);
+list.append(50);
+list.append(80);
+list.print();
+console.log("=========");
+list.removeFromFront();
+list.print();
+console.log("=========");
+list.removeFromEnd();
+list.print();
+console.log("=========");
