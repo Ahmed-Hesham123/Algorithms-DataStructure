1feba0046e1b633ff8145609bc356d980686d34d
Ahmed Hesham <ahmedh246891@gmail.com>
2023-10-10 19:13:37 +0300

    A new problem has been added

---
+++ LeetCode_PS/Thirty_Days_With_JS/14_Timeout_Cancellation/main.ts
@@ -0,0 +1,61 @@
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type Fn = (...args: JSONValue[]) => void;
+
+function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
+  let timer: any = setTimeout(() => {
+    fn(...args);
+    timer = null;
+  }, t);
+
+  const cancelFn = () => {
+    if (timer !== null) {
+      clearTimeout(timer);
+      timer = null;
+    }
+  };
+
+  return cancelFn;
+}
+
+const result = [];
+const fn = (x: number) => x * 5;
+const args = [2],
+  t = 20,
+  cancelT = 50;
+
+const cancel = cancellable(fn, args, t);
+
+console.log(cancel);
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 5
+ *  const args = [2], t = 20, cancelT = 50
+ *
+ *  const start = performance.now()
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)})
+ *  }
+ *
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelT)
+ *
+ *  setTimeout(() => {
+ *     cancel()
+ *  }, cancelT)
+ *
+ *  setTimeout(() => {
+ *     console.log(result) // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
+// Big-O = O(1) - Constant
